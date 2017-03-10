0053afc2e95e3f3701705a5552100b801dc04e24
tungptvn <tungpt.hd@gmail.com>
2017-03-10 21:40:56 +0700

    feat(data): add post model

---
+++ backend/AspNetWebApiBlog/AspNetWebApiBlog/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWebApiBlog.Models
+{
+    public class Post
+    {
+        public long Id { get; private set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public string Status { get; set; }
+        public string FeatureImage { get; set; }
+        public string Content { get; set; }
+        public DateTime PostDate { get; set; }
+        public long CategoryId { get; private set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser Author { get; set; }
+    }
+
+}
