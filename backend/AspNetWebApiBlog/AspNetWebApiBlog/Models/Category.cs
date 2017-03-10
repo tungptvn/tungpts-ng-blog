23d655fcecbfe777b09c3510fb0fde2c29758ad4
tungptvn <tungpt.hd@gmail.com>
2017-03-10 21:40:24 +0700

    feat(data): add post model

---
+++ backend/AspNetWebApiBlog/AspNetWebApiBlog/Models/Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWebApiBlog.Models
+{
+    public class Category
+    {
+        public long Id { get; private set; }
+        public string CategoryName { get; private set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public long? ParentCategoryId { get; private set; }
+        public virtual Category ParentCategory { get; private set; }
+        public virtual ICollection<Category> SubCategories { get; private set; }
+    }
+    public class CategoryConfiguration : EntityTypeConfiguration<Category>
+    {
+        public CategoryConfiguration()
+        {
+            this.HasKey(x => x.Id);
+
+            this.HasMany(x => x.SubCategories)
+            .WithOptional(y => y.ParentCategory)
+            .Map(m => m.MapKey("ParentCategoryId"));
+        }
+    }
+}
