eff51779878f425a738562f87e0006d2a6cc89e3
tungptvn <tungpt.hd@gmail.com>
2017-03-11 15:55:45 +0700

    chore(*)

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -1,14 +1,15 @@
-function ExampleCtrl() {
 
+function homeCtrl(postService) {
+ 'ngInject';
   // ViewModel
   const vm = this;
-
+  postService.get();
   vm.title = 'AngularJS, Gulp, and Browserify! Written with keyboards and love!';
   vm.number = 1234;
 
 }
 
 export default {
-  name: 'ExampleCtrl',
-  fn: ExampleCtrl
+  name: 'homeCtrl',
+  fn: homeCtrl
 };
