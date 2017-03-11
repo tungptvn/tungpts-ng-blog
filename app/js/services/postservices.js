d361d610fff2d63d4e787e353f45e472175c11f8
tungptvn <tungpt.hd@gmail.com>
2017-03-11 15:56:06 +0700

    feat(client): add posts data services

---
+++ app/js/services/postservices.js
@@ -0,0 +1,34 @@
+function postService(httpClient) {
+  'ngInject';
+
+  const service = {};
+
+  service.getBy = function (id) {
+    return new Promise((resolve, reject) => {
+      httpClient.get(`/posts/${id}`)
+        .then(res => resolve(res))
+        .catch(err => reject(err));
+    });
+  };
+
+  service.getAll = function () {
+    return new Promise((resolve, reject) => {
+      httpClient.get('/posts')
+        .then(res => resolve(res))
+        .catch(err => reject(err));
+    });
+  };
+  service.getByCategory = function (id) {
+    return new Promise((resolve, reject) => {
+      httpClient.get(`/posts/category/${id}`)
+        .then(res => resolve(res))
+        .catch(err => reject(err));
+    })
+  }
+
+  return service;
+}
+export default {
+  name: 'postService',
+  fn: postService
+};
