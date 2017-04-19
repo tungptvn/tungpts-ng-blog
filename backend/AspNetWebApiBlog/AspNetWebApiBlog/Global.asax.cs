028eca2fd58311c2c9bd3e83d1cefdc7364f8ffa
tungptvn <tungpt.hd@gmail.com>
2017-04-19 22:19:03 +0700

    feat(api): update database add discription post

--- backend/AspNetWebApiBlog/AspNetWebApiBlog/Global.asax.cs
+++ backend/AspNetWebApiBlog/AspNetWebApiBlog/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using AspNetWebApiBlog.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
