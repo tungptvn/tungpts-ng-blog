72f77a743513e4fdff0c089c8089f1075275351a
tungptvn <tungpt.hd@gmail.com>
2017-04-21 11:48:00 +0700

    chore(*): add admincp

--- app/js/on_config.js
+++ app/js/on_config.js
@@ -63,13 +63,13 @@ function OnConfig($stateProvider, $locationProvider, $urlRouterProvider, $compil
       }
     })
     .state('admin', {
-      url: 'admin',
+      url: '/admin',
       controller: 'adminCtrl as admin',
       templateUrl: 'admin/index.html',
       title: 'admin'
     })
     .state('admin.postMng', {
-      url: 'postMng/:Id',
+      url: '/postMng/:Id',
       controller: 'postMngCtrl as postMng',
       templateUrl: 'admin/postMng.html',
       title: 'postMng'
