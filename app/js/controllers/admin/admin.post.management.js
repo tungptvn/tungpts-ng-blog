bf6a0109a1a60e5e551a4a1ed9f689317d25af7f
tungptvn <tungpt.hd@gmail.com>
2017-05-24 21:20:51 +0700

    chore

--- app/js/controllers/admin/admin.post.management.js
+++ app/js/controllers/admin/admin.post.management.js
@@ -1,10 +1,39 @@
-function postMngCtrl(postList) {
+function postMngCtrl(postList,postService,$scope,$log) {
   'ngInject';
   // ViewModel
   const vm = this;
   vm.postList = postList;
   vm.searchText = 'asdas';
   vm.title = 'Posts Management';
+  vm.delete = function (Id) {
+    swal({
+        title: 'Are you sure?',
+        text: 'You will not be able to recover!',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#DD6B55',
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel plx!',
+        closeOnConfirm: false,
+        closeOnCancel: false
+      },
+      (isConfirm) => {
+        if (isConfirm) {
+          postService.delete(Id).then((res) => {
+            $log.debug("deleted", res);
+            swal('Deleted', 'The entity has been deleted :)', 'success');
+            postService.getAll().then(res => {
+              $scope.$apply(() => {
+                vm.catList = res;
+              })
+            });
+          }).catch(err => swal(`Error`, `${err}`, `error`))
+        } else {
+          swal('Cancelled', 'The entity has is safe :)', 'error');
+        }
+      });
+
+  }
 }
 
 export default {
--- backend/AspNetWebApiBlog/AspNetWebApiBlog/Controllers/PostsController.cs
+++ backend/AspNetWebApiBlog/AspNetWebApiBlog/Controllers/PostsController.cs
@@ -6,13 +6,13 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
 using System.Web.Http.Description;
 using AspNetWebApiBlog.Models;
 using System.Web.Http.Cors;
 using System.Security.Claims;
 using System.Web;
 using Microsoft.AspNet.Identity;
+using System.Web.Http;
 
 namespace AspNetWebApiBlog.Controllers
 {
